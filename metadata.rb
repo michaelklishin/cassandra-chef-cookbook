7070fa52fd17ba3a9d11937acfb7dd33eef2a96b
Caleb Groom <caleb@calebgroom.com>
2012-12-13 16:55:17 -0600

    cookbook version bump

--- metadata.rb
+++ metadata.rb
@@ -3,4 +3,4 @@ maintainer_email "michael@defprotocol.org"
 license          "Apache 2.0"
 description      "Installs/Configures OpsCode Apache Cassandra"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "1.0.0"
