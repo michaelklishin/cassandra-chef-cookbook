96e42b3d7b4973b60269bf67ff17a31ffe6b2eb2
Caleb Groom <caleb@calebgroom.com>
2012-12-13 16:43:11 -0600

    Add metadata.rb

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Michael S. Klishin"
+maintainer_email "michael@defprotocol.org"
+license          "Apache 2.0"
+description      "Installs/Configures OpsCode Apache Cassandra"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
