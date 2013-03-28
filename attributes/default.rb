dab489f8e549cbb159739bc743b1d05de35cfb51
breerly <grayson.koonce@kwarter.com>
2013-03-28 16:40:27 -0700

    more default attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -24,7 +24,15 @@ default[:cassandra] = {
   :conf_dir         => "/etc/cassandra/",
   # commit log, data directory, saved caches and so on are all stored under the data root. MK.
   :data_root_dir    => "/var/lib/cassandra/",
+  :commitlog_dir    => "/var/lib/cassandra/",
   :log_dir          => "/var/log/cassandra/",
-  :listen_address   => "localhost",
-  :rpc_address      => "localhost"
+  :listen_address   => node[:ipaddress],
+  :rpc_address      => node[:ipaddress],
+  :max_heap_size    => nil,
+  :heap_new_size    => nil,
+  :vnodes           => false,
+  :seeds            => [],
+  :concurrent_reads => 32,
+  :concurrent_writes => 32,
+  :snitch           => 'SimpleSnitch'
 }
