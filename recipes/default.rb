1db14ba7cf8c4ac5d546e3db238e36878fc8e1cd
Grayson Koonce <grayson.koonce@gmail.com>
2013-04-02 15:15:41 -0700

    Fixing template path

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,7 @@ include_recipe "cassandra::datastax"
 
 %w(cassandra.yaml cassandra-env.sh).each do |f|
   template File.join(node["cassandra"]["conf_dir"], f) do
-    source "cassandra/#{f}.erb"
+    source "#{f}.erb"
     owner node["cassandra"]["user"]
     group node["cassandra"]["user"]
     mode  0644
