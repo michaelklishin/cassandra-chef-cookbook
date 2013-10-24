e8f260247b9a4897255d2821daeed129a5a352a0
Michael Klishin <michael@clojurewerkz.org>
2013-10-24 04:44:46 +0400

    Bump version to 2.0

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
 name             "cassandra"
 maintainer       "Michael S. Klishin"
-maintainer_email "michael@defprotocol.org"
+maintainer_email "michael@clojurewerkz.org"
 license          "Apache 2.0"
 description      "Installs/Configures OpsCode Apache Cassandra"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.1"
+version          "2.0.0"
 
 depends "java"
 depends "apt"
