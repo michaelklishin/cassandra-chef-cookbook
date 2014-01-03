8eed1f61cb91d995725408f04d5f32ffa67de4fb
mihasya <m@mihasya.com>
2014-01-03 12:48:33 -0800

    bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "michael@defprotocol.org"
 license          "Apache 2.0"
 description      "Installs/Configures OpsCode Apache Cassandra"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.1"
+version          "1.0.2"
 
 depends "java"
 depends "apt"
