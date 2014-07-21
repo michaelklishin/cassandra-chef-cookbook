c05787d41a04d2fff8275f9a071c52e66f3451f2
vkhatri <vkhatri>
2014-07-21 09:25:01 +0530

    fixed issue caused by attribute file datastax.rb for conf_dir attr

--- attributes/datastax.rb
+++
@@ -1,5 +0,0 @@
-default[:cassandra][:conf_dir] = '/etc/cassandra/'
-
-# Datastax have this as default name, and C* starts after package is installed
-# so providing your own with force you to stop C* and clear data directories
-#
