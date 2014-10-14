9f8f7cec06d85c1e39580b688832a302a4b6daa8
Malcolm Jones <malcolm@adobe.com>
2014-10-14 15:13:42 -0400

    fix: switched back version to 2.8.0 @fix

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "michael@clojurewerkz.org"
 license          "Apache 2.0"
 description      "Installs/configures Apache Cassandra"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "2.9.0"
+version          "2.8.0"
 depends "java"
 depends "ulimit"
 depends "apt"
