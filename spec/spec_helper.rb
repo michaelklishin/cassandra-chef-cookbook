a0728390b456bcb8d5dc8ef911f4974b6b17fa1b
Malcolm Jones <malcolm@adobe.com>
2014-10-16 15:03:02 -0400

    feature: chefspec tests for dsc21 on centos 6.4. 67.65% coverage @feature

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,21 +2,6 @@ require 'chefspec'
 require 'chefspec/berkshelf'
 
 RSpec.configure do |config|
-  # Specify the path for Chef to find roles (default: [ascending search])
-  # config.role_path = "/var/roles"
-
-  # Specify the Chef log_level (default: :warn)
-  # config.log_level = :info
-
-  # Specify the path to a local JSON file with Ohai data (default: nil)
-  # config.path = "ohai.json"
-
-  # Specify the operating platform to mock Ohai data from (default: nil)
-  config.platform = 'centos'
-
-  # Specify the operating version to mock Ohai data from (default: nil)
-  config.version = '6.4'
-
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
