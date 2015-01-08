8c865ab366ee651933ea908bc1226417a68e14e1
Chris Henry <henry.christopher@gmail.com>
2015-01-07 20:11:27 -0500

    Add integration tests for the datastax agent.

---
+++ test/integration/opscenter-agent-datastax/serverspec/opscenter_datastax_agent_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe 'opscenter-datastax-agent' do
+
+  it 'installs, enables & runs the agent' do
+    expect(package 'datastax-agent').to be_installed
+    expect(service 'datastax-agent').to be_enabled
+    expect(service 'datastax-agent').to be_running
+  end
+
+end
