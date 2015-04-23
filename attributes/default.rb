430496cf9f8422e34778bdbbf8fe409863f2f990
will.salt <williamejsalt@gmail.com>
2015-04-23 16:49:40 +0100

    added opscenter agent conf_dir attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -219,4 +219,3 @@ default['cassandra']['opscenter']['agent']['use_chef_search'] = true
 default['cassandra']['opscenter']['agent']['server_role'] = 'opscenter_server'
 default['cassandra']['opscenter']['agent']['use_ssl'] = true
 default['cassandra']['opscenter']['agent']['conf_dir'] = '/var/lib/datastax-agent/conf'
-
