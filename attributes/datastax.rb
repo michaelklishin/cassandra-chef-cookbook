0f77e892c18b1aacffc8ad9aee4d8fb3308c10e8
Michael Klishin <michael@clojurewerkz.org>
2015-07-01 13:51:48 +0300

    Update package name for 2.1.x

--- attributes/datastax.rb
+++ attributes/datastax.rb
@@ -1,5 +1,5 @@
 
-default['cassandra']['package_name']  = 'dsc20'
+default['cassandra']['package_name']  = 'dsc21'
 default['cassandra']['release']       = '1'
 
 default['cassandra']['yum']['repo'] = 'datastax'
