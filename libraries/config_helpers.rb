09acac2377217ce7fda95b5646a5e3e8b796fe7d
Michael Klishin <michael@clojurewerkz.org>
2015-08-07 01:57:00 +0300

    Make seed discovery optional and more flexible as suggested by @vkhatri

--- libraries/config_helpers.rb
+++ libraries/config_helpers.rb
@@ -18,3 +18,35 @@ end
 def hash_to_yaml_string(hash)
   hash.to_hash.to_yaml
 end
+
+def discover_seed_nodes
+  # use chef search for seed nodes
+  if node['cassandra']['seed_discovery']['use_chef_search']
+    if Chef::Config[:solo]
+      Chef::Log.warn("Chef Solo does not support search, provide the seed nodes via node attribute node['cassandra']['seeds']")
+      node['ipaddress']
+    else
+      if node['cassandra']['seed_discovery']['search_query']
+        # user defined search query
+        xs = search(:node, node['cassandra']['seed_discovery']['search_query']).map(&:ipaddress).sort.uniq
+      else
+        # cookbook auto search query
+        search_query = "chef_environment:#{node.chef_environment} AND role:#{node['cassandra']['seed_discovery']['search_role']} AND cassandra_cluster_name:#{node['cassandra']['cluster_name']}"
+        xs = search(:node, search_query).map(&:ipaddress).sort.uniq
+      end
+
+      if xs.empty?
+        node['ipaddress']
+      else
+        xs.take(node['cassandra']['seed_discovery']['count']).join(',')
+      end
+    end
+  else
+    # user defined seed nodes
+    if node['cassandra']['seeds'].is_a?(Array)
+      node['cassandra']['seeds'].join(',')
+    else
+      node['cassandra']['seeds']
+    end
+  end
+end
