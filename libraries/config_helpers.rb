8e574d84ef67c6597f64b93b00bb8ee2ee6d4396
Michael Klishin <michael@clojurewerkz.org>
2015-08-21 03:18:40 +0300

    Simplify

--- libraries/config_helpers.rb
+++ libraries/config_helpers.rb
@@ -7,11 +7,13 @@ require 'yaml'
 
 def cassandra_bool_config(config_val)
   if config_val.is_a?(String)
-    return config_val
-  elsif config_val
-    return 'true'
+    config_val
   else
-    return 'false'
+    if config_val
+      'true'
+    else
+      'false'
+    end
   end
 end
 
