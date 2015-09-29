5f2d68e4bbb8258a93ac6e08e240eee3df7d9f55
Bill Warner <bwarner@demark.com>
2015-09-29 12:25:31 -0700

    rubocop fixes

--- spec/agent_datastax_spec.rb
+++ spec/agent_datastax_spec.rb
@@ -1,18 +1,13 @@
 require 'spec_helper'
 
 describe 'cassandra-dse::opscenter_agent_datastax' do
-
   let(:chef_run) do
-
     ChefSpec::SoloRunner.new(platform: 'centos', version: '6.4') do |node|
-
       node.set['cassandra']['cluster_name'] = 'test'
       node.set['cassandra']['version'] = '2.1.7'
       node.set['cassandra']['install_java'] = true
       node.set['cassandra']['yum']['options'] = '--always-have-options'
-
     end.converge(described_recipe)
-
   end
 
   it 'includes dependent recipes' do
@@ -32,5 +27,4 @@ describe 'cassandra-dse::opscenter_agent_datastax' do
   it 'renders the agent config file' do
     expect(chef_run).to create_template('/var/lib/datastax-agent/conf/address.yaml')
   end
-
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,22 +2,18 @@ require 'chefspec'
 require 'chefspec/berkshelf'
 
 RSpec.configure do |config|
-
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
 
   config.before(:each) do
-
     # stub out the repo_server data bag
     server_node = {
       'id' => 'node',
       'ipaddress' => '0.0.0.0'
     }
     stub_search(:node, 'roles:opscenter_server').and_return([server_node])
-
   end
-
 end
 
 at_exit { ChefSpec::Coverage.report! }
