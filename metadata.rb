96b100f6522e0d58dfac9b8342ab601f059c1003
Michael Klishin <michael@clojurewerkz.org>
2016-09-22 13:04:18 +0300

    Correct version in metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description 'Installs/configures Apache Cassandra'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url 'https://github.com/michaelklishin/cassandra-chef-cookbook' if respond_to?(:source_url)
 issues_url 'https://github.com/michaelklishin/cassandra-chef-cookbook/issues' if respond_to?(:issues_url)
-version '4.2.1'
+version '4.2.0'
 depends 'java'
 depends 'ulimit'
 depends 'apt'
