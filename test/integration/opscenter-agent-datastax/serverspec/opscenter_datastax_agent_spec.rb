904cefa39f9a3971f37f710789e9cfef8e10c683
Brian Skarda <brian.skarda@gmail.com>
2017-09-27 17:50:57 -0400

    Fix chefspec tests
    The removed end tag in the logback config was added in
    https://github.com/michaelklishin/cassandra-chef-cookbook/commit/4ed5a7d59a246980f686d4b87da9294eb824e89e
    through a revert that ended up with two end tags.
    
    Also fixes #359

--- test/integration/opscenter-agent-datastax/serverspec/opscenter_datastax_agent_spec.rb
+++ test/integration/opscenter-agent-datastax/serverspec/opscenter_datastax_agent_spec.rb
@@ -1,16 +1,13 @@
 require 'spec_helper'
 
 describe 'opscenter-datastax-agent' do
-  it 'installs, enables & runs the agent' do
+  context 'installs, enables & runs the agent' do
     describe package('datastax-agent') do
       it { should be_installed }
     end
 
     describe service('datastax-agent') do
       it { should be_enabled }
-    end
-
-    describe service('datastax-agent') do
       it { should be_running }
     end
   end
